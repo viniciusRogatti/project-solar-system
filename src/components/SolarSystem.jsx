7f5413527e08edf70b89c3609307a85be8a3e309
vinicius <vinnyzsouza@gmail.com>
2022-07-20 15:24:13 -0300

    requisito 1-2

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <p>TESTE</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
