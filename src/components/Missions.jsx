0f477be1d4d0ab7d4430f3dd0248b5576675563a
vinicius <vinnyzsouza@gmail.com>
2022-07-20 16:50:11 -0300

    finalizando projeto

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
+    const allMissions = missions.map((e, index) => (
+      <MissionCard
+        key={ index }
+        name={ e.name }
+        year={ e.year }
+        country={ e.country }
+        destination={ e.destination }
+      />
+    ));
     return (
       <div data-testid="missions">
-        <h1>  TESTE  </h1>
+        <Title headline="Missões" />
+        {allMissions}
       </div>
     );
   }
