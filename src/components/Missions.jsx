efc8a674176f2f395373f7beeb2edf605ee2ef9d
vinicius <vinnyzsouza@gmail.com>
2022-07-20 16:17:45 -0300

    requesito 7

---
+++ src/components/Missions.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <h1>  TESTE  </h1>
+      </div>
+    );
+  }
+}
+
+export default Missions;
